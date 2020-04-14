67fb6522a929ab1511e296093eeae6cfa9985697
Zilvinas Katinas <zilviscat@yahoo.com>
2020-04-14 18:22:17 +0300

    Trecia pamoka
    Iskelti globalus kintamieji

--- QA_Mokymai_VCS_Pamoka_0409/QA_Mokymai_VCS_Pamoka_0409/UnitTest1.cs
+++ QA_Mokymai_VCS_Pamoka_0409/QA_Mokymai_VCS_Pamoka_0409/UnitTest1.cs
@@ -12,8 +12,34 @@ namespace QA_Mokymai_VCS_Pamoka_0409
    
     public class Tests
     {
-        //private ChromeDriver driver;
+        //private ChromeDriver driver; - siauriau uz IWebDriver, nes tai tik chrome driveris
         private IWebDriver driver;
+
+        private By elementPopupCloseButtonSelector = By.CssSelector("#editable_popup[style*='display: block;'] .close");
+        private By elementAddToCartButtonSelector = By.Id("add2cart_button");
+
+        //"=>" tas pats, kas "get"
+        private IWebElement elementPopupCloseButton => driver.FindElement(elementPopupCloseButtonSelector);
+        private IWebElement elementPopupCloseButtonAfterLogin => driver.FindElement(elementPopupCloseButtonSelector);
+        private IWebElement elementAddToCartButton => driver.FindElement(elementAddToCartButtonSelector);
+
+        private const string LoginFormId = "login_form";
+
+        private IWebElement elementLoginIconButton => driver.FindElement(By.CssSelector(".need2login"));
+        private IWebElement elementEmailInput => driver.FindElement(By.CssSelector($"#{LoginFormId} [name='email']"));
+        private IWebElement elementPasswordInput => driver.FindElement(By.CssSelector($"#{LoginFormId} [name='password']"));
+        private IWebElement elementLoginButton => driver.FindElement(By.CssSelector($"#{LoginFormId} .btn-primary"));
+
+        private IList<IWebElement> visibleItems => driver.FindElements(By.CssSelector(".owl-item.active"));
+        private IList<IWebElement> selcetQuantity => driver.FindElements(By.CssSelector(".qty-select .plus"));
+
+        private string itemCountInCartBeforeBuy => driver.FindElement(By.CssSelector("em.cnt")).Text;
+        private string zeroItemInCart = "0";
+        private string itemCountInCartAfterBuy => driver.FindElement(By.CssSelector("em.cnt")).Text;
+        private string oneItemInCart = "1";
+
+
+
 
         [SetUp]
         public void Setup()
@@ -34,29 +60,37 @@ namespace QA_Mokymai_VCS_Pamoka_0409
             Thread.Sleep(2000);
             //Suranda pop lango "close" mygtuka ir ji paspaudzia
             //driver.FindElement(By.CssSelector("#editable_popup[style='display: block;'] .close")).Click();
-            IWebElement elementClosePopup = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("#editable_popup[style*='display: block;'] .close")));
-            elementClosePopup.Click();
+            //IWebElement elementPopupCloseButton = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("#editable_popup[style*='display: block;'] .close")));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(elementPopupCloseButtonSelector));            
+            elementPopupCloseButton.Click();
 
             Thread.Sleep(2000);
             //Randame "login" mygtuka pagal klase "need2login"
-            driver.FindElement(By.CssSelector(".need2login")).Click();
+            //IWebElement elementLoginIconButton = driver.FindElement(By.CssSelector(".need2login"));
+            elementLoginIconButton.Click();
 
-            Thread.Sleep(1000);
+            Thread.Sleep(1500);
             //Randame "email" laukeli pagal formos "id" ir pagal laukelio "name"
-            driver.FindElement(By.CssSelector("#login_form [name='email']")).SendKeys("testeris888@test.lt");
+            //IWebElement elementEmailInput = driver.FindElement(By.CssSelector($"#{LoginFormId} [name='email']"));
+            elementEmailInput.SendKeys("testeris888@test.lt");
 
             //Randame "password" laukeli pagal formos "id" ir pagal laukelio "name"
-            driver.FindElement(By.CssSelector("#login_form [name='password']")).SendKeys("testeris888");
+            //IWebElement elementPasswordInput = driver.FindElement(By.CssSelector($"#{LoginFormId} [name='password']"));
+            elementPasswordInput.SendKeys("testeris888");
 
             //Randame "Prisijungti" mygtuka pagal formos "id" ir pagal mygtuko klase "btn-primary"
-            driver.FindElement(By.CssSelector("#login_form .btn-primary")).Click();
+            //IWebElement elementLoginButton = driver.FindElement(By.CssSelector("#login_form .btn-primary"));
+            elementLoginButton.Click();
 
             Thread.Sleep(1000);
-            IWebElement elementClosePopupAfterLogin = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("#editable_popup[style*='display: block;'] .close")));
-            elementClosePopupAfterLogin.Click();
+            //IWebElement elementPopupCloseButtonAfterLogin = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("#editable_popup[style*='display: block;'] .close")));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(elementPopupCloseButtonSelector));
+            elementPopupCloseButtonAfterLogin.Click();
 
             Thread.Sleep(1000);
-            //driver.FindElement(By.CssSelector("#profile_menu .ico-profile")).Click();
+            //IWebElement elementUserIconAfterLogin = driver.FindElement(By.CssSelector("#profile_menu .ico-profile"));
+            //elementUserIconAfterLogin.Click();
+            //
             //Thread.Sleep(1000);
 
             //Patikrinti, ar prisijunges
@@ -74,26 +108,28 @@ namespace QA_Mokymai_VCS_Pamoka_0409
             TestLogin();
 
             //Patikriname, ar krepselyje nera prekiu
-            string itemCountInCartBeforeBuy = driver.FindElement(By.CssSelector("em.cnt")).Text;
-            string zeroItemInCart = "0";
+            //string itemCountInCartBeforeBuy = driver.FindElement(By.CssSelector("em.cnt")).Text;
+            //string zeroItemInCart = "0";
             Assert.AreEqual(itemCountInCartBeforeBuy, zeroItemInCart);
 
             //Parenkame pirma rodoma item
-            IList<IWebElement> visibleItems = driver.FindElements(By.CssSelector(".owl-item.active"));
+            //IList<IWebElement> visibleItems = driver.FindElements(By.CssSelector(".owl-item.active"));
             visibleItems[0].Click();
 
             //Parenkame item kieki
-            IList<IWebElement> selcetQuantity = driver.FindElements(By.CssSelector(".qty-select .plus"));
+            //IList<IWebElement> selcetQuantity = driver.FindElements(By.CssSelector(".qty-select .plus"));
             selcetQuantity[0].Click();
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("add2cart_button"))).Click();
+            //IWebElement elementAddToCartButton = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("add2cart_button")));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(elementAddToCartButtonSelector));            
+            elementAddToCartButton.Click();
 
             //Patikriname, ar item idetas i krepseli
-            System.Threading.Thread.Sleep(3000);
-            string itemCountInCartAfterBuy = driver.FindElement(By.CssSelector("em.cnt")).Text;
-            string oneItemInCart = "1";
+            Thread.Sleep(3500);
+            //string itemCountInCartAfterBuy = driver.FindElement(By.CssSelector("em.cnt")).Text;
+            //string oneItemInCart = "1";
             Assert.AreEqual(itemCountInCartAfterBuy, oneItemInCart);
             
-            System.Threading.Thread.Sleep(3000);
+            Thread.Sleep(3000);
         }
 
 
